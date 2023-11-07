3876abd16d1f56c102858c70d8511f39b326c885
HanakoSakura <vickysakura@dingtalk.com>
2023-11-07 18:29:54 +0800

    のろい

--- Player.py
+++ Player.py
@@ -1,12 +1,13 @@
 import pygame
 import json
+import time
 
 lrc = []
 lrcTrack = []
 
 def LoadSaikoLrc(file:str):
     global lrc,lrcTrack
-    with open(file,'r',encoding='utf-8') as f:
+    with open(file+'.saikolrc','r',encoding='utf-8') as f:
         tmp = json.loads(f.read())
         lrc = tmp[0]
         lrcTrack = tmp[1]
@@ -20,3 +21,25 @@ def Init():
     pygame.display.set_icon(HeadICO)
 
 # TODO:awa
+# 我实在不知道怎么写了
+# 这个代码先挂在这里了
+
+def ConsoleMain(file,SampleRate=64000):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file+'.wav')
+    pygame.mixer.music.set_volume(1.)
+    pygame.mixer.music.play()
+
+    start_time = time.time()
+    pointer = 0
+    while pygame.mixer.music.get_busy():
+        if pointer < len(lrcTrack):
+            if time.time()-start_time >= lrcTrack[pointer]/SampleRate:
+                print(lrc[pointer])
+                pointer+=1
+        pass
+
+if __name__ == '__main__':
+    import sys
+    LoadSaikoLrc(sys.argv[1])
+    ConsoleMain(sys.argv[1])
